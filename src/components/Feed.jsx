a8062bc4e111644e6cc2a2daf7fc9a7e151e0fef
Mariano Conti <nanexcool@gmail.com>
2018-05-27 09:40:00 -0300

    Add kovan hackathon feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,7 +10,23 @@ class Feed extends Component {
     const updated = this.props.updated ? moment.unix(this.props.updated).fromNow() : this.props.updated;
     const color = this.props.expires < 0 ? 'red' : ( this.props.expires < 2700 ? 'orange' : '#444');
     //const balance = web3.fromWei(this.props.balance).toFixed(3);
+    let provider = 'MakerDAO'
+    if (this.props.idx === 1) {
+      provider = 'Rhombus Network'
+    } else if (this.props.idx === 2) {
+      provider = '#ETHBuenosAires Hackathon Team!'
+    }
     return (
+      this.props.network === 'kovan' ?
+      <div>
+        <p>
+          <b>{this.props.idx + 1}</b> {value} <Link href={`https://${this.props.network === 'kovan' ? 'kovan.' : ''}etherscan.io/address/${this.props.address}`} text={this.props.address} />
+        </p>
+        <p>
+          Oracle provided by <b>{provider}</b>
+        </p>
+      </div>
+      :
       <div>
         <p style={{color}}>
           <b>{this.props.idx + 1}</b> {value} {this.props.valid && this.props.expires > 0 ? 'expires' : 'expired'} {expires}.{updated && `Updated ${updated}.`} <Link href={`https://${this.props.network === 'kovan' ? 'kovan.' : ''}etherscan.io/address/${this.props.address}`} text={this.props.address} />
